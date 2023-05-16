f282c283f928764d1b9c286106f69162ba2bd539
omercier <omercier@centreon.com>
2023-05-16 23:20:22 +0200

    Almost creating hostgroups

--- python/migration_v0/import_hostgroups.py
+++ python/migration_v0/import_hostgroups.py
@@ -1,5 +1,25 @@
 import json
 import pathlib
+import requests
+
+baseUrl = 'http://192.168.59.134/centreon/api/latest'
+login = 'admin'
+password = 'centreon'
+
+epAuth = '/login'
+
+json_credentials = {
+    "security": {
+        "credentials": {
+            "login": login,
+            "password": password
+        }
+    }
+}
+
+response = requests.post(baseUrl+epAuth, json=json_credentials)
+body = json.loads(str(response.content.decode()))
+print(str(body['security']['token']))
 
 
 fh = open(
@@ -9,3 +29,12 @@ fh = open(
 json_input = fh.read()
 fh.close()
 print(json_input)
+
+data_input = json.loads(json_input)
+
+for hg in data_input:
+    print(json.dumps(hg))
+    response = requests.post(baseUrl + '/configuration/hosts/groups', headers={"X-AUTH-TOKEN": str(body['security']['token'])}, json=json.dumps(hg))
+    body = json.loads(str(response.content.decode()))
+    print(str(body))
+
