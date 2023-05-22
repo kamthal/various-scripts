fb49f32589a8510432299f8bb046e39c9e87f47b
omercier <omercier@centreon.com>
2023-05-22 11:25:34 +0200

    Groups, categories and severities all OK

--- python/migration_v0/centreon_api.py
+++ python/migration_v0/centreon_api.py
@@ -47,19 +47,29 @@ def cv2api_createGeneric(serverAddress: str, endpointUrl, authToken, jsonData, c
     logging.debug("cv2api_createGeneric() ending")
     return(str(body))
 
-def cv2api_createHostGroup(serverAddress: str, hgJsonData, authToken, customUri: str = "centreon", serverProto: str = "http"):
-    return cv2api_createGeneric('192.168.59.134', '/configuration/hosts/groups', authToken, hgJsonData)
+def cv2api_createHostGroup(serverAddress: str, jsonData, authToken, customUri: str = "centreon", serverProto: str = "http"):
+    return cv2api_createGeneric('192.168.59.134', '/configuration/hosts/groups', authToken, jsonData)
 
-def cv2api_createHostCategory(serverAddress: str, hgJsonData, authToken, customUri: str = "centreon", serverProto: str = "http"):
-    return cv2api_createGeneric('192.168.59.134', '/configuration/hosts/categories', authToken, hgJsonData)
+def cv2api_createHostCategory(serverAddress: str, jsonData, authToken, customUri: str = "centreon", serverProto: str = "http"):
+    return cv2api_createGeneric('192.168.59.134', '/configuration/hosts/categories', authToken, jsonData)
 
-def cv2api_createHostSeverity(serverAddress: str, hgJsonData, authToken, customUri: str = "centreon", serverProto: str = "http"):
-    return cv2api_createGeneric('192.168.59.134', '/configuration/hosts/categories', authToken, hgJsonData)
+def cv2api_createHostSeverity(serverAddress: str, jsonData, authToken, customUri: str = "centreon", serverProto: str = "http"):
+    return cv2api_createGeneric('192.168.59.134', '/configuration/hosts/severities', authToken, jsonData)
+
+def cv2api_createServiceGroup(serverAddress: str, jsonData, authToken, customUri: str = "centreon", serverProto: str = "http"):
+    return cv2api_createGeneric('192.168.59.134', '/configuration/services/groups', authToken, jsonData)
+
+def cv2api_createServiceCategory(serverAddress: str, jsonData, authToken, customUri: str = "centreon", serverProto: str = "http"):
+    return cv2api_createGeneric('192.168.59.134', '/configuration/services/categories', authToken, jsonData)
+
+def cv2api_createServiceSeverity(serverAddress: str, jsonData, authToken, customUri: str = "centreon", serverProto: str = "http"):
+    return cv2api_createGeneric('192.168.59.134', '/configuration/services/severities', authToken, jsonData)
 
 def main():
     logging.debug('main() starting')
     logging.info("Authenticating")
     myToken = cv2api_authenticate('192.168.59.134')
+
     logging.info("Managing host groups")
     hg_data = json.loads(load_file(str(Path(__file__).parent.resolve()) + "/source_data/host_groups.json"))
     for hg in hg_data:
@@ -75,6 +85,23 @@ def main():
     hs_data = json.loads(load_file(str(Path(__file__).parent.resolve()) + "/source_data/host_severities.json"))
     for hs in hs_data:
         cv2api_createHostSeverity('192.168.59.134', hs, myToken)
+
+    logging.info("Managing service groups")
+    hg_data = json.loads(load_file(str(Path(__file__).parent.resolve()) + "/source_data/service_groups.json"))
+    for hg in hg_data:
+        #logging.debug(json.dumps(hg))
+        cv2api_createServiceGroup('192.168.59.134', hg, myToken)
+    
+    logging.info("Managing service categories")
+    hc_data = json.loads(load_file(str(Path(__file__).parent.resolve()) + "/source_data/service_categories.json"))
+    for hc in hc_data:
+        cv2api_createServiceCategory('192.168.59.134', hc, myToken)
+    
+    logging.info("Managing service severities")
+    hs_data = json.loads(load_file(str(Path(__file__).parent.resolve()) + "/source_data/service_severities.json"))
+    for hs in hs_data:
+        cv2api_createServiceSeverity('192.168.59.134', hs, myToken)
+
     logging.debug('main() ending')
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO, format='[%(asctime)s][%(levelname)s] %(message)s')
--- python/migration_v0/delete_all_hostgroups.py
+++ python/migration_v0/delete_all_hostgroups.py
@@ -1,5 +1,4 @@
 import json
-import pathlib
 import requests
 
 baseUrl = 'http://192.168.59.134/centreon/api/latest'
@@ -20,6 +19,7 @@ response = requests.post(baseUrl+epAuth, json=json_credentials)
 body = json.loads(str(response.content.decode()))
 authToken = str(body['security']['token'])
 post_headers = {"X-AUTH-TOKEN": authToken, "Accept": "*/*"}
+# Delete all host groups
 response = requests.get(baseUrl + '/configuration/hosts/groups', headers=post_headers)
 #body = json.loads(str(response.content.decode()))
 dec = response.content.decode()
@@ -29,8 +29,8 @@ for hg in hGroups:
     id = hg["id"]
     print("%s" % id )
     requests.delete(baseUrl + '/configuration/hosts/groups/' + str(id) , headers=post_headers)
+# Delete all host categories
 response = requests.get(baseUrl + '/configuration/hosts/categories', headers=post_headers)
-#body = json.loads(str(response.content.decode()))
 dec = response.content.decode()
 body = json.loads(dec)
 hCategories = body["result"]
@@ -38,3 +38,40 @@ for hc in hCategories:
     id = hc["id"]
     print("%s" % id )
     requests.delete(baseUrl + '/configuration/hosts/categories/' + str(id) , headers=post_headers)
+# Delete all host severities
+response = requests.get(baseUrl + '/configuration/hosts/severities', headers=post_headers)
+dec = response.content.decode()
+body = json.loads(dec)
+hSeverities = body["result"]
+for hs in hSeverities:
+    id = hs["id"]
+    print("%s" % id )
+    requests.delete(baseUrl + '/configuration/hosts/severities/' + str(id) , headers=post_headers)
+# Delete all service groups
+response = requests.get(baseUrl + '/configuration/services/groups', headers=post_headers)
+#body = json.loads(str(response.content.decode()))
+dec = response.content.decode()
+body = json.loads(dec)
+sGroups = body["result"]
+for sg in sGroups:
+    id = sg["id"]
+    print("%s" % id )
+    requests.delete(baseUrl + '/configuration/services/groups/' + str(id) , headers=post_headers)
+# Delete all service categories
+response = requests.get(baseUrl + '/configuration/services/categories', headers=post_headers)
+dec = response.content.decode()
+body = json.loads(dec)
+sCategories = body["result"]
+for sc in sCategories:
+    id = sc["id"]
+    print("%s" % id )
+    requests.delete(baseUrl + '/configuration/services/categories/' + str(id) , headers=post_headers)
+# Delete all service severities
+response = requests.get(baseUrl + '/configuration/services/severities', headers=post_headers)
+dec = response.content.decode()
+body = json.loads(dec)
+sSeverities = body["result"]
+for ss in sSeverities:
+    id = ss["id"]
+    print("%s" % id )
+    requests.delete(baseUrl + '/configuration/services/severities/' + str(id) , headers=post_headers)
